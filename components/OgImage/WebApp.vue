1432204ed86a56d89cc8aa53b5c7a8c0ce942eb9
Nii Aryeh <sup@niiaryeh.com>
2024-06-16 04:42:45 +0000

    ogimage size

--- components/OgImage/WebApp.vue
+++ components/OgImage/WebApp.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="h-full w-full flex justify-center items-center bg-black">
 		<div class="logo">
-			<img src="/logo-light.png" class="size-[100px]" alt="logo" />
+			<img src="/logo-light.png" class="size-[300px]" alt="logo" />
 		</div>
 	</div>
 </template>
